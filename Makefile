7e44d84c2d7ba7d2e596fd752a56193eff58d8cb
Bruno Bord <bruno@jehaisleprintemps.net>
2015-06-11 20:52:44 +0200

    optional output filename

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 all: html pdf
 
 html: style.css markdown.css jeu-troll.md
-	python build.py
+	python build.py index2.html
 
-pdf: index.html
+pdf: index-deux.html
 	pandoc index.html -o jeu-troll-brut.pdf
--- build.py
+++ build.py
@@ -1,9 +1,15 @@
+import sys
 import CommonMark
 import jinja2
 from codecs import open
 from jinja2 import Environment, FileSystemLoader
 
 if __name__ == '__main__':
+    print sys.argv
+    if len(sys.argv) >= 2:
+        output_file = sys.argv[1]
+    else:
+        output_file = 'index.html'
     parser = CommonMark.DocParser()
     renderer = CommonMark.HTMLRenderer()
     with open('jeu-troll.md', encoding='utf8') as fd:
@@ -15,5 +21,5 @@ if __name__ == '__main__':
     template = env.get_template('base.html')
     output = template.render(content=html)
 
-    with open('index.html', 'w', encoding='utf8') as fd:
+    with open(output_file, 'w', encoding='utf8') as fd:
         fd.write(output)
