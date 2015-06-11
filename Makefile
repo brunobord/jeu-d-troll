33e7cbff83adb750326096278819fa8c8be0da56
Bruno Bord <bruno@jehaisleprintemps.net>
2015-06-11 21:48:47 +0200

    pas menu dans pdf brut

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ all: html pdf
 
 html: style.css jeu-troll.md
 	python build.py index.html
+	python build.py print.html print.html
 
-pdf: index.html
-	pandoc index.html -o jeu-troll-brut.pdf
+pdf: print.html
+	pandoc print.html -o jeu-troll-brut.pdf -V geometry:margin=1in
--- build.py
+++ build.py
@@ -5,10 +5,16 @@ from codecs import open
 from jinja2 import Environment, FileSystemLoader
 
 if __name__ == '__main__':
+    output_file = 'index.html'
+    template_name = 'base.html'
+
     if len(sys.argv) >= 2:
         output_file = sys.argv[1]
-    else:
-        output_file = 'index.html'
+    if len(sys.argv) >= 3:
+        template_name = sys.argv[2]
+
+    print output_file, template_name
+
     parser = CommonMark.DocParser()
     renderer = CommonMark.HTMLRenderer()
     with open('jeu-troll.md', encoding='utf8') as fd:
@@ -17,7 +23,7 @@ if __name__ == '__main__':
     html = renderer.render(ast)
 
     env = Environment(loader=FileSystemLoader('templates'))
-    template = env.get_template('base.html')
+    template = env.get_template(template_name)
     output = template.render(content=html)
 
     with open(output_file, 'w', encoding='utf8') as fd:
